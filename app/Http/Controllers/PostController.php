e487e98efea7a6df6cbe4e586fe61a0caf1e74a5
Ekain Susperregi <suspekain@gmail.com>
2019-12-13 17:18:07 +0100

    crear post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,73 +9,82 @@ use App\Category;
 
 class PostController extends Controller
 {
-    public function index() {
-          $posts=Post::all();
-          return view('posts.index', ['posts' => $posts]);
-    }
+  public function index() {
+    $posts=Post::all();
+    return view('posts.index', ['posts' => $posts]);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create() {
-      $categorias = Category::all();
-      return view('posts.create', ['categorias'=>$categorias]);
-    }
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function create() {
+    $categorias = Category::all();
+    return view('posts.create', ['categorias'=>$categorias]);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request) {
+    $post = new Post;
+    $post->title = $request->input('title');
+    $post->excerpt = $request->input('excerpt');
+    $post->body = $request->input('body');
+    $post->image = $request->file('img');
+    $post->category_id = $request->get('category');
+    $post->user_id = Auth::user()->id;
+    $post->save();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id) {
-      return view('posts.post', ['post' => Post::find($id)]);
-    }
+    $posts = Post::where('user_id', Auth::user()->id)->get();
+    return redirect(route('post.index', ['posts'=>$posts]));
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  /**
+  * Display the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function show($id) {
+    return view('posts.post', ['post' => Post::find($id)]);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function edit($id)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, $id)
+  {
+    //
+  }
+
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy($id)
+  {
+    //
+  }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
 <br>
-<form method="POST" action="">
+<form method="POST" action="{{route('post.store')}}">
   @csrf
-  Titulo: <input type="text" name="title"><br>
-  Subtitulo: <input type="text" name="excerpt"><br>
-  Texto: <input type="text" name="body"><br>
-  Imagen: <input type="file" name="imagen"><br>
-  Categoria: <select name="category">
+  Titulo: <input type="text" name="title"><br><br>
+  Subtitulo: <input type="text" name="excerpt"><br><br>
+  Texto: <input type="text" name="body"><br><br>
+  Imagen: <input type="file" name="imagen"><br><br>
+  Categoria: <select name="category"><br>
     @foreach($categorias as $c)
     <option value="{{$c->id}}">{{$c->id}}</option>
     @endforeach
-  </select><br>
-  <button type="submit">Crear</button>
+  </select><br><br>
+  <button type="submit">Crear</button><br><br>
 </form>
 @endsection
