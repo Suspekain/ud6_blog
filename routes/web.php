494d0b36ede604f816e22e02ece94f2aa663d5a3
Ekain Susperregi <suspekain@gmail.com>
2019-12-16 18:16:51 +0100

    admin plantilla lehen pausoak

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('post', 'PostController')->only(['index', 'show', 'create', 'edit', 'store', 'update', 'destroy']);
+
+
+Route::get('/admin', function () {
+    return view('admin');
+});
