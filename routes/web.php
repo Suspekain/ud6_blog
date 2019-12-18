3251972ed99e26c124622bac2166f79c9f1407f6
Ekain Susperregi <suspekain@gmail.com>
2019-12-18 15:45:19 +0100

    delete + edit berriz ongi

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('post', 'PostController')->only(['index', 'show', 'create', 'edit', 'store', 'update', 'destroy']);
-
+Route::resource('post', 'PostController')->only(['index', 'show', 'create','store', 'edit', 'update', 'destroy']);
 
 Route::get('/admin', function () {
     return view('admin');
